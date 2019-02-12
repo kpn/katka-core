3b436230a6fab7891ea671ce29c20df9ada586c7
Michiel Holtkamp <michiel@elements.nl>
2019-02-12 08:47:41 +0000

    NEW - Added admin models

--- katka/admin.py
+++ katka/admin.py
@@ -1,13 +1,30 @@
 from django.contrib import admin
 
 from katka.fields import username_on_model
-from katka.models import Team
+from katka.models import Credential, CredentialSecret, Project, Team
 
 
-@admin.register(Team)
-class TeamAdmin(admin.ModelAdmin):
-    fields = ('name', 'group')
-
+class WithUsernameAdminModel(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         with username_on_model(self.model, request.user.username):
             super().save_model(request, obj, form, change)
+
+
+@admin.register(Team)
+class TeamAdmin(WithUsernameAdminModel):
+    fields = ('name', 'group')
+
+
+@admin.register(Project)
+class ProjectAdmin(WithUsernameAdminModel):
+    fields = ('name', 'slug', 'team')
+
+
+@admin.register(Credential)
+class CredentialAdmin(WithUsernameAdminModel):
+    fields = ('name', 'slug', 'credential_type', 'team')
+
+
+@admin.register(CredentialSecret)
+class CredentialSecretAdmin(WithUsernameAdminModel):
+    fields = ('key', 'value', 'credential')
